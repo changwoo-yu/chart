59baef79f234d98975800ecda2053242442c215b
changwoo-yu <changwooyu1213@gmail.com>
2024-07-05 18:47:21 +0900

    remove: 연습 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
-import Test1 from "@/components/Test1";
 import ExerAmountChart from "@/components/charts/exerAmountChart";
-import Example from "@/components/charts/inbodyChart";
 
 export default function Home() {
-  function test() {
-    return 5 + 4;
-  }
   return (
     <div>
       <ExerAmountChart />
-      {/* <Example /> */}
-      {/* <Test1 /> */}
-      {test()}
     </div>
   );
 }
